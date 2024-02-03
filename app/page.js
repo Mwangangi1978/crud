3ba1b0c281ae85ca22a4b911c51093eb53868d10
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-02-03 07:57:46 +0300

    api routes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-import Navbar from "@/components/NavBar";
+import Navbar from "./../components/NavBar";
+import TopicsList from './../components/TopicList'
+import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Heading textAlign={'center'} marginBottom={'10px'} marginTop={'10px'} color={'#EFB7BA'}>Fun Facts about Kenya</Heading>
+      <TopicsList/>
+    </>
   );
 }
